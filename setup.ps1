84793da84683a06d20ae164f11fafe647d3d2326
Daniel Scholl <dascholl@microsoft.com>
2022-04-11 12:02:49 -0500

    Updated

--- setup.ps1
+++ setup.ps1
@@ -7,7 +7,8 @@ $Packages = `
   'git', `
   'visualstudiocode', `
   'docker-desktop', `
-  'cascadiacode'
+  'cascadiacode', `
+  'powershell-core'
 
 
 # Install Packages
@@ -19,8 +20,9 @@ ForEach ($PackageName in $Packages)
 Add-LocalGroupMember -Group "docker-users" -Member "azureuser"
 
 # Install Azure PowerShell
-Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.216 -Force
 Set-ExecutionPolicy Bypass -Scope Process -Force; Install-Module -Name Az -AllowClobber -Scope AllUsers -Force
+Invoke-Expression "& { $(Invoke-RestMethod 'https://aka.ms/install-powershell.ps1') } -UseMSI -EnablePSRemoting -Quiet"
 
 # Install Azure CLI for Windows
 Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile .\AzureCLI.msi; Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet'
