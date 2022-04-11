1e6e27bc5a90c268a9f697a4f3e720962e453557
Daniel Scholl <dascholl@microsoft.com>
2022-04-11 09:04:48 -0500

    Updated ARM Template

--- setup.ps1
+++ setup.ps1
@@ -5,52 +5,50 @@ Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object
 # Assign Chocolatey Packages to Install
 $Packages = `
   'git', `
-  'microsoft-edge', `
   'visualstudiocode', `
-  'dotnetcore-sdk', `
   'docker-desktop', `
-  'powershell-core', `
   'cascadiacode'
 
 
 # Install Packages
 ForEach ($PackageName in $Packages)
-{ choco install $PackageName -y }
+{ choco install $PackageName -y 
+}
 
 # Add User to Docker Group
 Add-LocalGroupMember -Group "docker-users" -Member "azureuser"
 
-# Install Azure PowerShell
-Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
-Set-ExecutionPolicy Bypass -Scope Process -Force; Install-Module -Name Az -AllowClobber -Scope AllUsers -Force
+# # Install Azure PowerShell
+# Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+# Set-ExecutionPolicy Bypass -Scope Process -Force; Install-Module -Name Az -AllowClobber -Scope AllUsers -Force
 
-# Install Azure CLI for Windows
-Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile .\AzureCLI.msi; Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet'
+# # Install Azure CLI for Windows
+# Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile .\AzureCLI.msi; Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet'
 
-# Enable WSL
-Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux -NoRestart
+# # Enable WSL
+# Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux -NoRestart
 
-# Download and Install Ubuntu
-Invoke-WebRequest -Uri https://aka.ms/wsl-ubuntu-1804 -OutFile ~/Ubuntu.appx -UseBasicParsing
-Add-AppxPackage -Path ~/Ubuntu.appx
+# # Download and Install Ubuntu
+# Invoke-WebRequest -Uri https://aka.ms/wsl-ubuntu-1804 -OutFile ~/Ubuntu.appx -UseBasicParsing
+# Add-AppxPackage -Path ~/Ubuntu.appx
 
-# Download AdoptOpenJDK
-Invoke-WebRequest https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u242-b08/OpenJDK8U-jdk_x64_windows_hotspot_8u242b08.msi -OutFile ~/Java.msi -UseBasicParsing
+# # Download AdoptOpenJDK
+# Invoke-WebRequest https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u242-b08/OpenJDK8U-jdk_x64_windows_hotspot_8u242b08.msi -OutFile ~/Java.msi -UseBasicParsing
 
-# Download Windows Terminal
-Invoke-WebRequest https://github.com/microsoft/terminal/releases/download/v0.10.781.0/Microsoft.WindowsTerminal_0.10.781.0_8wekyb3d8bbwe.msixbundle -OutFile ~/Terminal.msixbundle -UseBasicParsing
+# # Download Windows Terminal
+# Invoke-WebRequest https://github.com/microsoft/terminal/releases/download/v0.10.781.0/Microsoft.WindowsTerminal_0.10.781.0_8wekyb3d8bbwe.msixbundle -OutFile ~/Terminal.msixbundle -UseBasicParsing
 
-# Download Cascadia Font
-Invoke-WebRequest https://github.com/microsoft/cascadia-code/releases/download/v1911.21/CascadiaPL.ttf -Outfile ~/CascadiaPL.ttf -UseBasicParsing
+# # Download Cascadia Font
+# Invoke-WebRequest https://github.com/microsoft/cascadia-code/releases/download/v1911.21/CascadiaPL.ttf -Outfile ~/CascadiaPL.ttf -UseBasicParsing
 
-# Bring down Desktop Shortcuts
-$zipDownload = "https://github.com/danielscholl/hol-win10/blob/master/shortcuts.zip?raw=true"
-$downloadedFile = "D:\shortcuts.zip"
-$vmFolder = "C:\Users\Public\Desktop"
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-Invoke-WebRequest $zipDownload -OutFile $downloadedFile
-Add-Type -assembly "system.io.compression.filesystem"
-[io.compression.zipfile]::ExtractToDirectory($downloadedFile, $vmFolder)
+# # Bring down Desktop Shortcuts
+# $zipDownload = "https://github.com/danielscholl/hol-win11/blob/master/shortcuts.zip?raw=true"
+# $downloadedFile = "D:\shortcuts.zip"
+# $vmFolder = "C:\Users\Public\Desktop"
+# [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+# Invoke-WebRequest $zipDownload -OutFile $downloadedFile
+# Add-Type -assembly "system.io.compression.filesystem"
+# [io.compression.zipfile]::ExtractToDirectory($downloadedFile, $vmFolder)
 
-# Reboot
-Restart-Computer
+# # Reboot
+# Restart-Computer
