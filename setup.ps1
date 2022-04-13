c119488b2a5d1df9a6c4adcb187d47fb976935fd
Daniel Scholl <dascholl@microsoft.com>
2022-04-13 10:19:13 -0500

    Added AZ powershell modules

--- setup.ps1
+++ setup.ps1
@@ -22,9 +22,10 @@ ForEach ($PackageName in $Packages)
 Add-LocalGroupMember -Group "docker-users" -Member "azureuser"
 
 # Install Azure PowerShell
-# Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.216 -Force
-# Set-ExecutionPolicy Bypass -Scope Process -Force; Install-Module -Name Az -AllowClobber -Scope AllUsers -Force
 # Invoke-Expression "& { $(Invoke-RestMethod 'https://aka.ms/install-powershell.ps1') } -UseMSI -EnablePSRemoting -Quiet"
+Set-ExecutionPolicy Bypass -Scope Process -Force; Install-Module -Name PackageManagement -RequiredVersion 1.1.0.0
+Set-ExecutionPolicy Bypass -Scope Process -Force; Install-Module -Name Az -AllowClobber -Scope AllUsers -Force
+
 
 # Install Azure CLI for Windows
 Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile .\AzureCLI.msi; Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet'
